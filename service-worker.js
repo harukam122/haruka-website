e9ee953ec64005e77a452ecce494537bf1fa6b32
harukam122 <mashar0122.yhm@gmail.com>
2022-05-31 15:53:04 +0900

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/haruka-website/precache-manifest.bf000a20f2aacd8474c3bf8aafb73fc3.js"
+  "/haruka-website/precache-manifest.0809c45f86331701d5ae7312a74bfb10.js"
 );
 
 self.addEventListener('message', (event) => {
